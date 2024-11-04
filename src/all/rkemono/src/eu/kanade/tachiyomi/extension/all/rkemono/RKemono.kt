b016a9a19c256357d13242192ad92d6620a37810
Sai Krishna Kalyan <anonymous190310099527@gmail.com>
2024-11-05 00:45:34 +0530

    .

--- src/all/rkemono/src/eu/kanade/tachiyomi/extension/all/rkemono/RKemono.kt
+++ src/all/rkemono/src/eu/kanade/tachiyomi/extension/all/rkemono/RKemono.kt
@@ -1,4 +1,4 @@
-package eu.kanade.tachiyomi.extension.all.hentaidad
+package eu.kanade.tachiyomi.extension.all.rkemono
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.model.Filter
@@ -7,38 +7,45 @@ import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
+import eu.kanade.tachiyomi.util.asJsoup
 import okhttp3.Request
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class HentaiDad() : ParsedHttpSource() {
-    override val baseUrl = "https://hentaidad.com"
+class RKemono() : ParsedHttpSource() {
+    override val baseUrl = "https://kemono.su"
     override val lang = "all"
-    override val name = "HentaiDad"
+    override val name = "RKemono"
     override val supportsLatest = true
 
     // Latest
     override fun latestUpdatesFromElement(element: Element): SManga {
         val manga = SManga.create()
-        manga.thumbnail_url = element.select("img .entry-image").attr("abs:src")
-        manga.title = element.select("img .entry-image").attr("abs:title")
-        manga.setUrlWithoutDomain(element.select("a").attr("abs:href"))
+        manga.thumbnail_url = element.select("div.post-card__image-container > img.post-card__image").attr("abs:src")
+        manga.title = element.select("header.post-card__header").text()
+        manga.setUrlWithoutDomain(element.select("article > a").attr("abs:href"))
         return manga
     }
 
-    override fun latestUpdatesNextPageSelector() = ".pagination-next:not([disabled])"
-
-    override fun latestUpdatesRequest(page: Int): Request {
-        return GET("$baseUrl/$page")
+    override fun latestUpdatesNextPageSelector() {
+        return "#paginator-top .pagination-button-after-current:not(.pagination-button-disabled)"
     }
 
-    override fun latestUpdatesSelector() = ".row > article"
+    override fun latestUpdatesRequest(page: Int): Request {
+        return GET("$baseUrl/api/v1/posts?o=${(($page - 1) * 50)}")
+    }
+
+    override fun latestUpdatesSelector() {
+        return ".card-list__items > article.post-card"
+    }
 
     // Popular
     override fun popularMangaFromElement(element: Element) = latestUpdatesFromElement(element)
     override fun popularMangaNextPageSelector() = latestUpdatesNextPageSelector()
     override fun popularMangaRequest(page: Int): Request {
-        return GET("$baseUrl/popular/$page")
+        return GET("$baseUrl/api/v1/posts/popular?o=${(($page - 1) * 50)}")
     }
     override fun popularMangaSelector() = latestUpdatesSelector()
 
@@ -49,7 +56,7 @@ class HentaiDad() : ParsedHttpSource() {
     override fun searchMangaRequest(page: Int, query: String, filters: FilterList): Request {
         val tagFilter = filters.findInstance<TagFilter>()!!
         return when {
-            query.isNotEmpty() -> GET("$baseUrl/search?tag=$query&page=$page")
+            query.isNotEmpty() -> GET("$baseUrl/api/v1/posts?o=${((page - 1) * 50)}&q=$query")
             else -> popularMangaRequest(page)
         }
     }
@@ -58,11 +65,11 @@ class HentaiDad() : ParsedHttpSource() {
     // Details
     override fun mangaDetailsParse(document: Document): SManga {
         val manga = SManga.create()
-        manga.title = document.select(".video-title h1").text()
+        manga.title = document.select("header.post__header > div.post__info > h1 > span").first().text()
         manga.description = document.select(".video-title h1").text().trim()
         val genres = mutableListOf<String>()
-        document.select(".description-box").first()!!.select("p > .video-tag").forEach {
-            genres.add(it.text().substringAfter("#"))
+        document.select("section#post-tags > div").first()!!.select("a").forEach {
+            genres.add(it.text())
         }
         manga.genre = genres.joinToString(", ")
         return manga
@@ -71,7 +78,7 @@ class HentaiDad() : ParsedHttpSource() {
     override fun chapterFromElement(element: Element): SChapter {
         val chapter = SChapter.create()
         chapter.chapter_number = 0F
-        chapter.name = element.select(".video-title .text-capitalize").text()
+        chapter.name = element.select("title").text()
         return chapter
     }
 
@@ -81,8 +88,8 @@ class HentaiDad() : ParsedHttpSource() {
 
     override fun pageListParse(document: Document): List<Page> {
         val pages = mutableListOf<Page>()
-        document.select(".lightgallery img").forEach {
-            val itUrl = it.attr("abs:src")
+        document.select("div.post__files a").forEach {
+            val itUrl = it.attr("href")
             pages.add(Page(pages.size, "", itUrl))
         }
         return pages
